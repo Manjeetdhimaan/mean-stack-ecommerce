42049517d6c9b7c52650400e1c73f4bebaf55e21
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2023-02-20 21:53:32 +0530

    orderSchema init

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -1,20 +1,63 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    name: {
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'OrderItem',
+        required: [true, 'Order Item is required']
+    }],
+    address: {
+        type: Object,
+        shippingAddress1: {
+            type: String,
+            required: [true, 'Shipping address is required']
+        },
+        shippingAddress2: {
+            type: String
+        },
+        city: {
+            type: String,
+            required: [true, 'City is required']
+        },
+        zip: {
+            type: String,
+            required: [true, 'Zip code is required']
+        },
+        country: {
+            type: String,
+            required: [true, 'Shipping country is required']
+        },
+        phone: {
+            type: String,
+            required: [true, 'Contact number is required']
+        }
+    },
+    status: {
         type: String,
-        required: [true, 'Product name is required']
+        required: true,
+        default: 'Pending'
     },
-    imageUrl: {
-        type: String,
-        required: [true, 'Product image is required']
+    totolPrice: {
+        type: Number
     },
-    countInStock: {
-        type: Number,
-        required: [true, 'Product stock quantity is required']
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     },
+    dateOrdered: {
+        type: Date,
+        default: Date.now
+    }
 }, {
     timestamps: true
+});
+
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true
 });
 
 mongoose.model('Order', orderSchema);
