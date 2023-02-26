02467a08acca92f9b6f934a967d62a94a7a783af
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2023-02-26 19:47:12 +0530

    admin panel init:Angular

--- client/manjeet-ecommerce/apps/admin/src/app/app.module.ts
+++ client/manjeet-ecommerce/apps/admin/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ShellComponent,
+    SidebarComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
