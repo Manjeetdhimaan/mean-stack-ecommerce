405866611593cd2f45d9e45be8dfd949998552d8
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2023-02-26 17:03:35 +0530

    added PrimeNg:Angular

--- client/manjeet-ecommerce/apps/ng-shop/src/app/app.module.ts
+++ client/manjeet-ecommerce/apps/ng-shop/src/app/app.module.ts
@@ -1,29 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {AccordionModule} from 'primeng/accordion';
+import { UiModule } from '@manjeet-ecommerce/ui';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { UiModule } from '@manjeet-ecommerce/ui';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NxWelcomeComponent,
     HomeComponent,
     ProductListComponent,
     HeaderComponent,
-    FooterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    UiModule
+    UiModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent],
